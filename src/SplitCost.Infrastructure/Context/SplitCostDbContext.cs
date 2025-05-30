7594e837efa8d5e4627a8ea0533afb115297e8b1
Hiro Matsumoto <hiroyukims@hotmail.com>
2025-05-30 11:30:16 +0900

    Rename database entities

--- src/SplitCost.Infrastructure/Context/SplitCostDbContext.cs
+++ src/SplitCost.Infrastructure/Context/SplitCostDbContext.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SplitCost.Domain.Common;
-using SplitCost.Domain.Entities;
+using SplitCost.Infrastructure.Entities;
 
 namespace SpitCost.Infrastructure.Context;
 
 public class SplitCostDbContext : DbContext
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Residence> Residences { get; set; }
-    public DbSet<Member> Members { get; set; }
-    public DbSet<Expense> Expenses { get; set; }
-    public DbSet<ExpenseShare> ExpenseShares { get; set; }
-    public DbSet<Address> Addresses { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<ResidenceEntity> Residences { get; set; }
+    public DbSet<MemberEntity> Members { get; set; }
+    public DbSet<ExpenseEntity> Expenses { get; set; }
+    public DbSet<ExpenseShareEntity> ExpenseShares { get; set; }
+    public DbSet<AddressEntity> Addresses { get; set; }
 
     public SplitCostDbContext(DbContextOptions<SplitCostDbContext> options)
         : base(options)
@@ -23,68 +23,68 @@ public class SplitCostDbContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         // USER ⇄ RESIDENCEMEMBER (Many-to-Many via Entity)
-        modelBuilder.Entity<Member>()
+        modelBuilder.Entity<MemberEntity>()
             .HasKey(rm => rm.Id);
 
-        modelBuilder.Entity<Member>()
+        modelBuilder.Entity<MemberEntity>()
             .HasOne(rm => rm.User)
             .WithMany(u => u.Residences)
             .HasForeignKey(rm => rm.UserId)
             .OnDelete(DeleteBehavior.Cascade);
 
-        modelBuilder.Entity<Member>()
+        modelBuilder.Entity<MemberEntity>()
             .HasOne(rm => rm.Residence)
             .WithMany(r => r.Members)
             .HasForeignKey(rm => rm.ResidenceId)
             .OnDelete(DeleteBehavior.Cascade);
 
         // RESIDENCE ⇄ RESIDENCEEXPENSE (One-to-Many)
-        modelBuilder.Entity<Expense>()
+        modelBuilder.Entity<ExpenseEntity>()
             .HasOne(e => e.Residence)
             .WithMany(r => r.Expenses)
             .HasForeignKey(e => e.ResidenceId)
             .OnDelete(DeleteBehavior.Cascade);
 
         // USER ⇄ RESIDENCEEXPENSE (RegisteredBy)
-        modelBuilder.Entity<Expense>()
+        modelBuilder.Entity<ExpenseEntity>()
             .HasOne(e => e.RegisteredBy)
             .WithMany(u => u.Expenses)
             .HasForeignKey(e => e.RegisteredByUserId)
             .OnDelete(DeleteBehavior.Restrict);
 
         // USER ⇄ RESIDENCEEXPENSE (PaidBy)
-        modelBuilder.Entity<Expense>()
+        modelBuilder.Entity<ExpenseEntity>()
             .HasOne(e => e.PaidBy)
             .WithMany(u => u.ResidenceExpensesPaid)
             .HasForeignKey(e => e.PaidByUserId)
             .OnDelete(DeleteBehavior.Restrict);
 
         // RESIDENCEEXPENSE ⇄ RESIDENCEEXPENSESHARE (One-to-Many)
-        modelBuilder.Entity<ExpenseShare>()
+        modelBuilder.Entity<ExpenseShareEntity>()
             .HasOne(s => s.Expense)
             .WithMany(e => e.Shares)
             .HasForeignKey(s => s.ExpenseId)
             .OnDelete(DeleteBehavior.Cascade);
 
         // USER ⇄ RESIDENCEEXPENSESHARE (One-to-Many)
-        modelBuilder.Entity<ExpenseShare>()
+        modelBuilder.Entity<ExpenseShareEntity>()
             .HasOne(s => s.User)
             .WithMany(u => u.ExpenseShares)
             .HasForeignKey(s => s.UserId)
             .OnDelete(DeleteBehavior.Restrict);
 
         // RESIDENCE
-        modelBuilder.Entity<Residence>()
+        modelBuilder.Entity<ResidenceEntity>()
             .HasOne(r => r.CreatedBy)
             .WithMany()
             .HasForeignKey(r => r.CreatedByUserId)
             .OnDelete(DeleteBehavior.Restrict);
 
         // ADDRRESS ⇄ RESIDENCE (One-to-One)
-        modelBuilder.Entity<Residence>()
+        modelBuilder.Entity<ResidenceEntity>()
         .HasOne(r => r.Address)
         .WithOne(a => a.Residence)
-        .HasForeignKey<Residence>(r => r.AddressId);
+        .HasForeignKey<ResidenceEntity>(r => r.AddressId);
 
     }
 
